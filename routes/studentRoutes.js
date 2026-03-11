0474c2e051627a45ec7728afe271cc9b36d17ff1
Ananya Mahajan <ananyam054@gmail.com>
2026-03-11 11:52:18 +0530

    Updated

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -7,12 +7,8 @@ router.get("/students", controller.getStudents);
 
 router.post("/add-student", controller.addStudent);
 
-router.delete("/students/:id", controller.deleteStudent);
-
 router.get("/ranking", controller.getRanking);
 
-router.put("/students/:id", controller.updateStudent);
-
-router.get("/topper", controller.getTopper);
+router.delete("/students/:roll", controller.deleteStudent);
 
 module.exports = router;
